114f84c042d878b15ab6c42ce1dc423f37a698e7
clouldlin <clouldlin@gmail.com>
2015-09-13 16:23:37 +0900

    브랜치 테스트 및 통합 합류

--- js/git/git.js
+++ js/git/git.js
@@ -1,7 +1,5 @@
 $(function() {
 	console.log( "===============" );
 	console.log( "ready!" );
-	console.log( "This is Branch!" );
-	console.log( "This is Merged!" );
 	console.log( "===============" );
 });
