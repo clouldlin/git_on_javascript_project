c4c4ea31ac48825a617cd6b69a7ffe73298c47a0
clouldlin <clouldlin@gmail.com>
2015-09-13 16:03:53 +0900

    실험 - 로그라인 추가

--- js/git/git.js
+++ js/git/git.js
@@ -1,5 +1,7 @@
 $(function() {
+	console.log( "===============" );
 	console.log( "ready!" );
 	console.log( "This is Branch!" );
 	console.log( "This is Merged!" );
+	console.log( "===============" );
 });
